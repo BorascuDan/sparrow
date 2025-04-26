12f64fbb735855b7ae4cda5d01a83773aac4a5f7
borascudan <borascu.dan24@gmail.com>
2025-04-26 16:49:39 +0300

    eroare

--- mobile/src/main/java/com/example/googleapp/ApiServices.kt
+++ mobile/src/main/java/com/example/googleapp/ApiServices.kt
@@ -3,6 +3,8 @@ package com.example.googleapp.api
 import com.example.googleapp.models.*
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
@@ -11,4 +13,7 @@ interface ApiService {
 
     @POST("api/users/login")
     fun loginUser(@Body loginRequest: LoginRequest): Call<ApiResponse<Map<String, User>>>
+
+    @GET("api/users/get")
+    fun getUserData(@Header("Authorization") token: String): Call<ApiResponse<User>>
 }
